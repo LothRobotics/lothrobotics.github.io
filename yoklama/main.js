459757725a1f5bc6e785a0d94e2b12a1f6bd1f6d
Baran Önen <baranonenn@gmail.com>
2021-12-31 13:29:26 +0300

    Push

--- yoklama/main.js
+++ yoklama/main.js
@@ -1,21 +1,22 @@
 "use strict"
 
-var eventName
-
 var team = ["Abdullah Efe Atabek", "Ahmet Doğu Tuna", "Ahmet Furkan Tekin", "Ahmet Kankılıç", "Ahmet Yahya Turhan", "Arda Akarsu", "Arhan Kuzey Girgin", "Ayşe Ümmühan Yakıcı", "Bekir Efe Kaya", "Bora Özüdoğru", "Buğra Aydın", "Burak Nezir Altun", "Deniz Arda Kozak", "Deniz Yergin", "Doruk Doğru", "Ebrar Dinç", "Efe Aslan", "Efe Aslan Gürkan", "Elif Çolafoğlu", "Elif Ünal", "Emir Karabulut", "Enes Salih Sezer", "Eray Güleşen", "Erdem Uyan", "Halit Hamza Şeker", "Hilal Tekin", "İbrahim Volkan Gök", "İremnur Özcan", "Kadir Bayra Balaban", "Kağan Zengin", "Mehmet Akif Albayrak", "Muhammed Bilal Öztürk", "Özgür Ozan Güralp", "Seda Vardar", "Sueda Aydın", "Turan Doruk Cerit", "Vefa Ceylan", "Yusuf Adıgüzel", "Yusuf Koymatoğlu", "Zeynep Güler", "Talha Batuhan CEYLAN","Yavuz Selim Şeremetli"]
+var date = new Date()
 
 function start() {
-	eventName = document.getElementById("eventname").value
 	document.getElementById("firstpage").style.display = "none"
 	document.getElementById("secondpage").style.display = "block"
 	document.getElementById("thirdpage").style.display = "none"
-	if (eventName != "") {
-		document.getElementById("eventheader").innerHTML = eventName + "<br>Yoklama"
+	document.getElementById("dateheader").innerHTML = date.toLocaleDateString('tr-TR')
+	if (document.getElementById("eventname").value != "") {
+		document.getElementById("eventnameheader").innerHTML = document.getElementById("eventname").value
+	} else {
+		document.getElementById("eventnameheader").innerHTML = "Kulüp Toplantısı"
 	}
 	team.forEach(e => {
-		document.getElementById("secondpage").innerHTML += '<label for="'+e+'">'+e+'</label> <input type="checkbox" name="'+e+'" id="'+e+'"><hr></hr>'
+		document.getElementById("secondpage").innerHTML += '<label for="'+e+'" class="container">'+e+'<input name="'+e+'" id="'+e+'" type="checkbox"><span class="checkmark"></span></label>'
 	});
-	document.getElementById("secondpage").innerHTML += '<button onclick="end()">Yoklamayı Sonlandır</button>'
+	document.getElementById("secondpage").innerHTML += '<button id="endbtn" onclick="end()">Yoklamayı sonlandır</button>'
 }
 
 function end() {
@@ -25,6 +26,7 @@ function end() {
 }
 
 function savepdf() {
+	document.getElementById("endbtn").style.display = "none"
 	document.getElementById("firstpage").style.display = "none"
 	document.getElementById("secondpage").style.display = "block"
 	document.getElementById("thirdpage").style.display = "none"
