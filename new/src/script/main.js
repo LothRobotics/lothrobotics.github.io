0a8e9dfc6c529b044c512ea6eeb2d5453152c4d0
N0t3nl1sh <shalithamza@gmail.com>
2022-12-15 14:41:30 +0300

    push

--- new/src/script/main.js
+++ new/src/script/main.js
@@ -1,9 +1,7 @@
 const testimonialsText = document.getElementById("testimonialsText"),
-  testimonialList = ["'It's not about doing, it's all about making'", "'Great power comes with great'"];
+  testimonialList = [
+    "'It's not about doing, it's all about making'",
+    "'Great power comes with great'",
+  ];
 
-function changeTestimonials() {
-  for (let i = 0; i < testimonialList.length; i++) {
-    const testimonialItem = testimonialList[i];
-    testimonialsText.innerHTML = testimonialItem
-  }
-}
+function changeTestimonials() {}
