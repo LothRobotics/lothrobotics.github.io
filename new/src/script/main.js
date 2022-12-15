55840df4d148b1479b0531dc59c8d2ae929b6d28
Wragnardev <shalithamza@gmail.com>
2022-12-15 21:35:30 +0300

    push

--- new/src/script/main.js
+++ new/src/script/main.js
@@ -1,7 +1,30 @@
-const testimonialsText = document.getElementById("testimonialsText"),
-  testimonialList = [
-    "'It's not about doing, it's all about making'",
-    "'Great power comes with great'",
-  ];
+const testimonialsText = document.getElementById("testimonialsText");
+const testimonialsList = [
+  "'It's not about doing, it's all about making'",
+  "'Great power comes with great responsibility'",
+];
+const testimonialsListLength = testimonialsList.length;
+let counter = testimonialsList.indexOf(testimonialsText.innerHTML);
 
-function changeTestimonials() {}
+function windowOnload() {
+  testimonialsText.innerHTML = testimonialsList[0];
+  counter = testimonialsList.indexOf(testimonialsText.innerHTML);
+}
+
+function changeTestimonialsForwards() {
+  if (counter < testimonialsListLength - 1) {
+    console.log(counter)
+    counter++;
+    console.log(counter)
+    testimonialsText.innerHTML = testimonialsList[counter];
+  }
+}
+
+function changeTestimonialsBackwards() {
+  if (counter > 0) {
+    console.log(counter)
+    counter--;
+    console.log(counter)
+    testimonialsText.innerHTML = testimonialsList[counter];
+  }
+}
