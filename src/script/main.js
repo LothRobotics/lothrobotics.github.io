6629a4c1dbe80c0059301c50a6f1134ca68b5570
N0t3nl1sh <shalithamza@gmail.com>
2022-12-23 11:35:20 +0300

    push

--- src/script/main.js
+++ src/script/main.js
@@ -1,14 +1,29 @@
 const sloganText = document.getElementById("slogan-text");
-
+let counter = 0;
 const sloganList = [
   '"It is not about doing it is all about making"',
-  "Great power comes with great responsiblty",
+  '"Great power comes with great responsiblty"',
 ];
+const listLength = sloganList.length;
 
 function windowOnLoad() {
   sloganText.innerHTML = sloganList[0];
 }
 
-function changeSloganBackwards() {}
+function changeSloganBackwards() {
+  if (counter <= 0) {
+    counter = listLength - 1;
+  } else if (counter > 0) {
+    counter--;
+  }
+  sloganText.innerHTML = sloganList[counter];
+}
 
-function changeSloganForwards() {}
+function changeSloganForwards() {
+  if (counter >= listLength - 1) {
+    counter = 0;
+  } else if (counter < listLength - 1) {
+    counter++;
+  }
+  sloganText.innerHTML = sloganList[counter];
+}
